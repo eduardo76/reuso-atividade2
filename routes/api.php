106a042c95ce2f3aa45180d4590277ce44e56005
Eduardo Tavares <etavaresf@gmail.com>
2017-12-13 07:18:58 -0300

    Comentário das rotas

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// api/orders
 Route::get('orders', 'OrdersController@orders');
 Route::post('orders', 'OrdersController@store');
+
+// api/orders/{order}
 Route::get('orders/{order}', 'OrdersController@show');
 Route::put('orders/{order}', 'OrdersController@update');
 
+//api/costumers
 Route::get('costumers', 'CostumersController@costumers');
 Route::post('costumers', 'CostumersController@store');
+
+//api/costumers/{costumer}
 Route::get('costumers/{costumer}', 'CostumersController@show');
 Route::get('costumers/{costumer}/orders', 'CostumersController@orders');
